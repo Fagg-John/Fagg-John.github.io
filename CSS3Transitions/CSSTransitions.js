021a8f7de3c3c657371d29ffb44f1eb34d3862b3
Fagg-John <byufan@me.com>
2017-05-16 08:55:08 -0600

    Update CSSTransitions.js

--- CSS3Transitions/CSSTransitions.js
+++ CSS3Transitions/CSSTransitions.js
@@ -1,6 +1,6 @@
 function myFunction() {
-    document.getElementById("myDIV").style.WebkitTransition = "all 2s"; // Code for Safari 3.1 to 6.0
-    document.getElementById("myDIV").style.transition = "all 2s";
+    document.getElementById("myCOUGARS").style.WebkitTransition = "all 2s"; // Code for Safari 3.1 to 6.0
+    document.getElementById("myCOUGARS").style.transition = "all 2s";
 }
 
 
