734efcd5bf011104f395ba291294be6b0778a132
Fagg-John <byufan@me.com>
2017-05-16 19:18:49 -0600

    Update CSSTransitions.js

--- CSS3Transitions/CSSTransitions.js
+++ CSS3Transitions/CSSTransitions.js
@@ -1,18 +1,4 @@
 function myFunction() {
-    document.getElementById("myCOUGARS").style.WebkitTransition = "all 2s"; // Code for Safari 3.1 to 6.0
+    document.getElementById("myCOUGARS").style.WebkitTransition = "all 2s";
     document.getElementById("myCOUGARS").style.transition = "all 2s";
 }
-
-
-/* Test Code - Remarked out
-
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
-
-function nextSlide(){
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
-}
-*/
