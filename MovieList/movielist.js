22a1b26de17daac9cf83446e50c468f50b8e063e
Fagg-John <byufan@me.com>
2017-05-26 20:03:56 -0600

    Update movielist.js

--- MovieList/movielist.js
+++ MovieList/movielist.js
@@ -4,18 +4,18 @@ var movies = [];
   function retrieveItem() {
     if (localStorage.moviesList) {
       items = JSON.parse(localStorage.getItem('moviesList'));
-      addRows(items);
+      AddRows(items);
     }
   }
 
-  function addRows(items){
+  function AddRows(items){
     for (var i in items) {
       var item = items[i];
       var table = document.getElementById("data_table");
       var table_len = (table.rows.length) - 1;
       var row = table.insertRow(table_len).outerHTML = "<tr id='row" + table_len + "'><td id='movieTitle_row" + table_len + "'>" + item.Product + "</td><td id='genre_row" + table_len + "'>" + item.Genre + "</td><td id='submittedBy_row" + table_len + "'>" + item.Submitted + "</td><td><input type='button' value='Delete' class='smallbutton' onclick='delete_row(" + table_len + ")'></td></tr>";
     }
-  } //addRows
+  } //AddRows
 
   function delete_row(no) {
     document.getElementById("row" + no + "").outerHTML = "";
@@ -93,6 +93,6 @@ var movies = [];
   function GotData(data) {
     if (HR.readyState === XMLHttpRequest.DONE) {
       var items = JSON.parse(data.target.response)
-      addRows(items);
+      AddRows(items);
     }
   } //GotData
