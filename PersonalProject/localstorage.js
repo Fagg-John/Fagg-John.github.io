45becc0eb4c012d317ea59273b27d35de51193af
Fagg-John <byufan@me.com>
2017-07-10 20:29:53 -0600

    Update localstorage.js

--- PersonalProject/localstorage.js
+++ PersonalProject/localstorage.js
@@ -1,1 +1,21 @@
+var score = [];
+function createLocalStorageKey(name)
+{
+  localStorage.setItem('player_name',name); 
+}
 
+function saveName(name)
+{
+  if(name=='')
+  {
+    alert("Name can't be blank");
+   return 0;
+  }
+	var div_name = document.getElementById('div-name');
+	var div_show_name = document.getElementById('div-show-name');
+	div_name.style.display = 'none';
+	div_show_name.style.display = 'block';
+	document.getElementById("pname").value = name;
+	document.getElementById("showname").innerHTML = name;
+	createLocalStorageKey(name);
+}
